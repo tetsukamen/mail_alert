cc39be95ff11613553f4b38e4a52fca92687027f
tetsuyawada <tetsukamen00@gmail.com>
2020-06-05 19:56:12 +0900

    macbook pro に環境構築

---
+++ app/Http/Controllers/AlertController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AlertController extends Controller{
+    public function index(){
+        return "Hello, World.";
+    }
+}
